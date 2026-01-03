5182981973d5997df9551be8a0cbd1291499ed59
Lemonzy <lemonzy.pro@gmail.com>
2026-01-03 03:35:46 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,37 +1,36 @@
 {
-  description = "NixOS environment";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    devShell.${system} = with pkgs;
-      mkShell {
-        buildInputs = with pkgs; [
-          clang
-          # Replace llvmPackages with llvmPackages_X, where X is the latest
-          # LLVM version (at the time of writing, 16)
-          llvmPackages_16.bintools
-          mold
-          pkg-config
-          rustup
-          yq-go
-        ];
-
-        shellHook = ''
-          export RUSTC_VERSION=$(yq ".toolchain.channel" rust-toolchain.toml)
-          export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-          export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-          rustup target add wasm32-unknown-unknown
-          rustup component add rust-analyzer
-        '';
-      };
-  };
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              # Rust dependencies
+              (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; })
+              pkg-config
+            ];
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          };
+      }
+    );
 }
